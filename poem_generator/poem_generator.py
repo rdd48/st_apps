61b49f23e472dcdfa5f433f0fc21df86237d942f
Robby Divine <rdivine48@gmail.com>
2022-07-24 09:38:47 -0700

    user can add own poems

--- poem_generator/poem_generator.py
+++ poem_generator/poem_generator.py
@@ -18,7 +18,6 @@ def remove_punc(l):
     return new_l
 
 def poem_to_words(txt_file):
-    '''train test split + split lines from input'''
     saved_words = []
     with open(txt_file, 'r') as f:
         lines = f.readlines()
@@ -31,18 +30,29 @@ def poem_to_words(txt_file):
                     saved_words.append(word)
     return saved_words
 
-def poem_to_list(txt_file):
-    '''train test split + split lines from input'''
+def poem_to_list(txt_file, user_file):
     saved_lines = []
-    with open(txt_file, 'r') as f:
-        lines = f.readlines()
-        for l in lines:
+
+    if not user_file:
+        with open(txt_file, 'r') as f:
+            lines = f.readlines()
+            for l in lines:
+                # check that line has data
+                if len(l.strip()) > 0:
+                    # split and remove punctuation
+                    split_l = remove_punc(l)
+                    saved_lines.append(split_l)
+    
+    else:
+        for l in txt_file.split('\n'):
             # check that line has data
             if len(l.strip()) > 0:
                 # split and remove punctuation
                 split_l = remove_punc(l)
                 saved_lines.append(split_l)
+    
     return saved_lines
+               
 
 def generate_models(poet_list):
 
@@ -161,12 +171,18 @@ def generate_sentence(max_len, pi_norm, A1_norm, A2_norm, word2int):
                 word_sentence += f' {int2word[following_word]}'
 
     return word_sentence
+    
 
 def generate_poem(models, num_lines, max_line_len):
+    sentences = []
     for _ in range(num_lines):
         sentence = generate_sentence(max_line_len, *models)
-        st.markdown(sentence.capitalize())
+        # st.markdown(sentence.capitalize())
+        sentences.append(sentence.capitalize())
+    st.session_state.poem = sentences
 
+def reset_state():
+    st.session_state.poem = None
 
 st.set_page_config(
     page_title='Poetry Generator',
@@ -174,10 +190,21 @@ st.set_page_config(
     layout='wide'
 )
 
+st.title(' :scroll: Poetry Generator :scroll: ')
+st.markdown('**Set options in the sidebar**')
+
+if 'poem' not in st.session_state:
+    st.session_state.poem = None
+
+# sidebar
 st.sidebar.header('Select options:')
-poet_selector = st.sidebar.selectbox('Select poet:', ('Shel Silverstein', 'Edgar Allan Poe', 'Robert Frost', 'Learn from my poems'))
+poet_selector = st.sidebar.selectbox('Select poet:', ('Shel Silverstein', 'Edgar Allan Poe', 'Robert Frost', 'Learn from my poems'), on_change=reset_state)
 num_lines = st.sidebar.slider('Number of lines', min_value=1, max_value=8, value=4, step=1)
 max_line_len = st.sidebar.slider('Maximum number of words per sentence', min_value=2, max_value=14, value=8, step=1)
+
+if st.session_state.poem:
+    for line in st.session_state.poem:
+        st.markdown(line)
 
 if poet_selector:
     if poet_selector != 'Learn from my poems':
@@ -188,11 +215,15 @@ if poet_selector:
         }
 
         poet_list = poem_to_list(f'./poem_generator/poem_inputs/{author2file[poet_selector]}.txt')
+        # poet_list = poem_to_list(f'./poem_inputs/{author2file[poet_selector]}.txt', user_file=False)
         models = models_from_poet(poet_list)
-        st.button('Generate poem!', on_click=generate_poem, args=[models, num_lines, max_line_len])
         
-    else:
+    else: 
         user_poems = st.text_area('Upload poems to learn from')
-        pass
-        # poet_list = poem_to_list()
-        # models = models_from_poet(poet_list)
+        # st.markdown(user_poems)
+        poet_list = poem_to_list(user_poems, user_file=True)
+        models = models_from_poet(poet_list)
+    
+    st.button('Generate poem!', on_click=generate_poem, args=[models, num_lines, max_line_len])
+
+
