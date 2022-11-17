f5c4c6bf39cbb87c6a2cc6530b40459d082704d5
Robby Divine <rdivine48@gmail.com>
2022-11-16 18:59:34 -0800

    prettifying graphs

--- lefi/graph_lefi_output.py
+++ lefi/graph_lefi_output.py
@@ -1,64 +1,138 @@
 import streamlit as st
-import pandas as pd
+import pickle
 import plotly.express as px
 import plotly.graph_objects as go
 
-
-from out import cdma_blast_cdhit_data as data
-
-# num_seqs, score_by_len, score_by_id, score_by_edge
-
-# df = pd.DataFrame(data.num_seqs)
-# df.head()
-
-seq_len_hist = px.bar(
-    # df,
-    x = data.num_seqs.keys(),
-    y = data.num_seqs.values(),
-    labels={'x':'Sequence Length', 'y':'Count'})
-
-score_len_hist = go.Figure()
-for k in sorted(data.score_by_len.keys()):
-    score_len_hist.add_trace(go.Box(
-        y=data.score_by_len[k],
-        name=k,
-    ))
-score_len_hist.update_layout(
-    xaxis={'title': 'Alignment Score'},
-    yaxis={'title': 'Alignment Length'}
+st.set_page_config(
+    page_title='Local EFI',
+    page_icon=':atom_symbol:',
+    layout='wide'
 )
 
-score_id_hist = go.Figure()
-for k in sorted(data.score_by_id.keys()):
-    score_id_hist.add_trace(go.Box(
-        y=data.score_by_id[k],
-        name=k,
-    ))
-score_id_hist.update_layout(
-    xaxis={'title': 'Alignment Score'},
-    yaxis={'title': 'Percent Identity'}
-)
+def resize_by_num_boxes(d, box_nums):
+    max_xaxis = max(d.keys())
+    if box_nums >= max_xaxis:
+        return d
 
-reverse_scores = sorted(data.score_by_edge.keys(), reverse=True)
-y_edges = []
-curr_edge_num = 0
-for s in reverse_scores:
-    curr_edge_num += data.score_by_edge[s]
-    y_edges.append(curr_edge_num)
+    box_size = max_xaxis // box_nums
+    if box_size == 1:
+        return d
 
-align_score_line = px.line(
-    x = reverse_scores,
-    y = y_edges,
-    labels={'x':'Alignment Score', 'y':'Number of Edges'})
+    resized_d = {(k // box_size) * box_size: 0 for k in range(0, max_xaxis, box_size)}
 
-edge_count_hist = px.bar(
-    x = data.score_by_edge.keys(),
-    y = data.score_by_edge.values(),
-    labels={'x':'Alignment Score', 'y':'Number of Edges'})
+    for k, v in d.items():
+        resized_d[(k // box_size) * box_size] += v
+    
+    return resized_d
+
+def resize_by_num_boxes_boxplot(d, box_nums):
+    max_xaxis = max(d.keys())
+    if box_nums >= max_xaxis:
+        return d
+
+    box_size = max_xaxis // box_nums
+    if box_size == 1:
+        return d
+
+    resized_d = {(k // box_size) * box_size: [] for k in range(0, max_xaxis, box_size)}
+
+    for k, v in d.items():
+        new_k = (k // box_size) * box_size
+        v_copy = resized_d[new_k].copy()
+        v_copy += v
+        resized_d[new_k] = v_copy
+    
+    return resized_d
+
+uploaded_pickle = st.file_uploader('Upload the .pickle output from the initial dataset generation.')
+
+if uploaded_pickle is not None:
+    data = pickle.load(uploaded_pickle)
+
+    # bin_size = st.sidebar.slider(
+    #     'Select the number of bins for the histograms', 
+    #     min_value=10, 
+    #     max_value=max(data['num_seqs'].keys()) // 2,
+    #     value=50 
+    # )
+
+    bin_size = st.sidebar.number_input(
+        'Select the number of bins for the histograms', 
+        min_value=10, 
+        max_value=max(data['num_seqs'].keys()) // 2,
+        value=50 
+    )
+
+    resized_ns = resize_by_num_boxes(data['num_seqs'], bin_size)
+    seq_len_hist = px.bar(
+        x = resized_ns.keys(),
+        y = resized_ns.values(),
+        # x = data['num_seqs'].keys(),
+        # y = data['num_seqs'].values(),
+        labels={'x':'Sequence Length', 'y':'Count'})
+
+    score_len_hist = go.Figure()
+    resized_sbl = resize_by_num_boxes_boxplot(data['score_by_len'], bin_size)
+    for k in sorted(resized_sbl.keys()):
+        score_len_hist.add_trace(go.Box(
+            y=resized_sbl[k],
+            name=k,
+            marker_color='royalblue',
+        ))
+    # for k in sorted(data['score_by_len'].keys()):
+    #     score_len_hist.add_trace(go.Box(
+    #         y=data['score_by_len'][k],
+    #         name=k,
+    #         marker_color='royalblue',
+    #     ))
+    score_len_hist.update_layout(
+        xaxis={'title': 'Alignment Score'},
+        yaxis={'title': 'Alignment Length'},
+        showlegend=False
+    )
+
+    score_id_hist = go.Figure()
+    resized_sbi = resize_by_num_boxes_boxplot(data['score_by_id'], bin_size)
+    for k in sorted(resized_sbi.keys()):
+        score_id_hist.add_trace(go.Box(
+            y=resized_sbi[k],
+            name=k,
+            marker_color='royalblue',
+        ))
+    # for k in sorted(data['score_by_id'].keys()):
+    #     score_id_hist.add_trace(go.Box(
+    #         y=data['score_by_id'][k],
+    #         name=k,
+    #         marker_color='royalblue',
+    #     ))
+    score_id_hist.update_layout(
+        xaxis={'title': 'Alignment Score'},
+        yaxis={'title': 'Percent Identity'},
+        showlegend=False
+    )
+
+    
+    reverse_scores = sorted(data['score_by_edge'].keys(), reverse=True)
+    y_edges = []
+    curr_edge_num = 0
+    for s in reverse_scores:
+        curr_edge_num += data['score_by_edge'][s]
+        y_edges.append(curr_edge_num)
+
+    align_score_line = px.line(
+        x = reverse_scores,
+        y = y_edges,
+        labels={'x':'Alignment Score', 'y':'Number of Edges'})
+
+    resized_sbe = resize_by_num_boxes(data['score_by_edge'], bin_size)
+    edge_count_hist = px.bar(
+        x = resized_sbe.keys(),
+        y = resized_sbe.values(),
+        labels={'x':'Alignment Score', 'y':'Number of Edges'})
 
 
-st.plotly_chart(seq_len_hist)
-st.plotly_chart(score_len_hist)
-st.plotly_chart(score_id_hist)
-st.plotly_chart(align_score_line)
-st.plotly_chart(edge_count_hist)
+    st.plotly_chart(seq_len_hist)
+    st.plotly_chart(score_len_hist)
+    st.plotly_chart(score_id_hist)
+    st.plotly_chart(align_score_line)
+    st.plotly_chart(edge_count_hist)
--- lefi/lefi.py
+++ lefi/lefi.py
@@ -1,11 +1,13 @@
 import sys
 import math
 import subprocess
+import pickle
+from Bio import Align
 
 fasta_file = sys.argv[1]
 
 global blast_path 
-blast_path = '/Users/rddivine/opt/miniconda3/envs/bp/bin/'
+blast_path = '/Users/robbydivine/opt/miniconda3/envs/lefi/bin/'
 
 def process_fasta(fasta_file):
     with open(fasta_file) as f:
@@ -40,6 +42,12 @@ def process_fasta(fasta_file):
     
     return fasta_dict
 
+# def calc_bitscore(raw_align_score, l, k):
+#     # S = (λ × S' − lnK)/ ln2
+#     # lambda, K = 0.3176, 0.134
+#     # lambda, K = 0.297, 0.082
+#     return ((l * raw_align_score) - math.log(k)) / math.log(2)
+
 def fasta_to_dicts(input_fasta):
 
     # keep info for xgmml
@@ -47,8 +55,8 @@ def fasta_to_dicts(input_fasta):
 
     fasta_dict = process_fasta(input_fasta)
 
-    subprocess.run(f'{blast_path}makeblastdb -in {input_fasta} -dbtype prot'.split())
-    out = subprocess.check_output(f"{blast_path}blastp -query {input_fasta} -db {input_fasta} -matrix BLOSUM62 -outfmt '6 qseqid qlen sseqid slen bitscore length pident'", shell=True)
+    # subprocess.run(f'{blast_path}makeblastdb -in {input_fasta} -dbtype prot'.split())
+    out = subprocess.check_output(f"{blast_path}blastp -query {input_fasta} -subject {input_fasta} -matrix BLOSUM62 -outfmt '6 qseqid qlen sseqid slen bitscore length pident'", shell=True)
 
     for line in out.decode('utf-8').split('\n'):
         if line:
@@ -64,18 +72,47 @@ def fasta_to_dicts(input_fasta):
 
             if n1 != n2:
 
+                # https://github.com/EnzymeFunctionInitiative/EFITools/blob/master/lib/EFI/Util/AlignmentScore.pm
+                # is formula from ^ actually?:
+                align_score = -(math.log(float(l1) * float(l2)) / math.log(10)) + (float(bitscore) * math.log(2) / math.log(10)) # log must be ln
+
                 # check for case where bitscore is so high that 2 ** -bitscore evaluates to 0.0
-                if (2 ** (-float(bitscore))):
-                    align_score = -math.log10((2. ** (-float(bitscore))) * float(l1) * float(l2))
-                else:
-                    align_score = -math.log10((2. ** (-float(1000.))) * float(l1) * float(l2))
+                # if (2 ** (-float(bitscore))):
+                #     align_score = -math.log10((2. ** (-float(bitscore))) * float(l1) * float(l2))
+                # else:
+                #     align_score = -math.log10((2. ** (-float(1000.))) * float(l1) * float(l2))
                 
                 # edges dict structure = (name 1, name 2): (percent id, align_score, align_len)
                 if (n1, n2) and (n2, n1) not in edges:
                     edges[(n1, n2)] = (pident, align_score, align_len)
-                
 
-    
+                # aligner = Align.PairwiseAligner()
+                # aligner.match_score = 1
+                # aligner.mismatch_score = 1
+                # align = aligner.align("MEAREATATGESCMRVDAIAKVTGRARYTDDYVMAGMCYAKYVRSPIAHGYAVSINDEQARSLPGVLAIFTWEDVPDIPFATAGHAWTLDENKRDTADRALLTRHVRHHGDAVAIVVARDELTAEKAAQLVSIEWQELPVITTPEAALAEDAAPIHNGGNLLKQSTMSTGNVQQTIDAADYQIQGHYQTPVIQHCHMESVTSLAWMEDDSRITIVSSTQIPHIVRRVVGQALDIPWSCVRVIKPFVGGGFGNKQDVLEEPMAAFLTSKLGGIPVKVSLSREECFLATRTRHAFTIDGQMGVNRDGTLKGYSLDVLSNTGAYASHGHSIASAGGNKVAYLYPRCAYAYSSKTCYTNLPSAGAMRGYGAPQVVFAVESMLDDAATALGIDPVEIRLRNAAREGDANPLTGKRIYSAGLPECLEKGRKIFEWEKRRAECQNQQGNLRRGVGVACFSYTSNTWPVGVEIAGARLLMNQDGTINVQSGATEIGQGADTVFSQMVAETVGVPVSDVRVISTQDTDVTPFDPGAFASRQSYVAAPALRSAALLLKEKIIAHAAVMLHQSAMNLTLIKGHIVLVERPEEPLMSLKDLAMDAFYHPERGGQLSAESSIKTTTNPPAFGCTFVDLTVDIALCKVTINRILNVHDSGHILNPLLAEGQVHGGMGMGIGWALFEEMIIDAKSGVVRNPNLLDYKMPTMPDLPQLESAFVEINEPQSAYGHKSLGEPPIIPVAAAIRNAVKMATGVAINTLPLTPKRLYEEFHLAGLI", "MSVDQVLTPKFVGTSVARREDPRLLTGRGRFVDDIAMPGMLHAQFVRSTVAAGHITGLDVGDVAGVEGVHRVFTAADLDLRPIRAELSRPLSEFVPTDMPVLAHDVVRYVGEPLAIVVAADAYSVEDGLEAARVSYRTVTAVTSADQAISEGVPLVHDSVPNNTVVDVQMFATEGIDEIFHDAHTVVSVQSRTGRQNALPLETRGCIAYWDDRDEQLIIYICTQVPHQVRTVTAQCLGLDERQVRVVVPDMGGGFGQKCVVGREEIAVAAAALKLRRPVKWIEDRKDALTASFLAREQQYDVRAAFDSEGHILGLDADVVCDMGAYSCYPFTAGIEPLMASAEMPGVYQVPAYRVRGRSVFSNKAPTAPYRGVSRPQYVMVMERLFERAARELQLDAVEIRRRNVITAFPYTGVNNITYDPGSYLEALNLCEQTLRDGGWYDLQERATADGRHIGIGYSCFSERTGYGSSAFAARKMNVVPGFDISEVRMDTSGTVVVTTGTMSHGQSHETTMAQIVADRLGITVDQVKIVQGDTDRITYGFGSFASRSITIGGSAVALASTKLGDKLCEIAAHLLETRDDNVELASGRVRQRDDHSKYVTYRDIADVAYLKAQLLPKGVEPGLSATASFDVFNDGTFSNATHGVVVELHEGTGQVEILKYVCVEDCGVAINPKIVEGQCRGGIAQGIAGALFEQVSYDENGNPLCASFIDYKVPTACEIPDIEIHHLETPCLFTESGAKGAGEGGTIGAPAAVLNAVNDGLRSTGVELNDTPITPVAVQAALAKGRP")
+                
+                # {(double) INT2_MAX, (double) INT2_MAX, (double) INT2_MAX, 0.3176, 0.134, 0.4012, 0.7916, -3.2, 0.623757, 4.964660, 4.964660},
+                # {11, 2, (double) INT2_MAX, 0.297, 0.082, 0.27, 1.1, -10, 0.641766, 12.673800, 12.757600},
+                # {10, 2, (double) INT2_MAX, 0.291, 0.075, 0.23, 1.3, -15, 0.649362, 16.474000, 16.602600},
+                # {9, 2, (double) INT2_MAX, 0.279, 0.058, 0.19, 1.5, -19, 0.659245, 22.751900, 22.950000},
+                # {8, 2, (double) INT2_MAX, 0.264, 0.045, 0.15, 1.8, -26, 0.672692, 35.483800, 35.821300},
+                # {7, 2, (double) INT2_MAX, 0.239, 0.027, 0.10, 2.5, -46, 0.702056, 61.238300, 61.886000},
+                # {6, 2, (double) INT2_MAX, 0.201, 0.012, 0.061, 3.3, -58, 0.740802, 140.417000, 141.882000},
+                # {13, 1, (double) INT2_MAX, 0.292, 0.071, 0.23, 1.2, -11, 0.647715, 19.506300, 19.893100},
+                # {12, 1, (double) INT2_MAX, 0.283, 0.059, 0.19, 1.5, -19, 0.656391, 27.856200, 28.469900},
+                # {11, 1, (double) INT2_MAX, 0.267, 0.041, 0.14, 1.9, -30, 0.669720, 42.602800, 43.636200},
+                # {10, 1, (double) INT2_MAX, 0.243, 0.024, 0.10, 2.5, -44, 0.693267, 83.178700, 85.065600},
+                # {9, 1, (double) INT2_MAX, 0.206, 0.010, 0.052, 4.0, -87, 0.731887, 210.333000, 214.842000},
+                # lambdas = [0.3176, 0.297, 0.291, 0.279, 0.264, 0.239, 0.201, 0.292, 0.283, 0.267, 0.243, 0.206]
+                # ks = [0.134, 0.082, 0.075, 0.058, 0.045, 0.027, 0.012, 0.071, 0.059, 0.041, 0.024, 0.010]
+
+                # for l, k in zip(lambdas, ks):
+                #     new_bitscore = calc_bitscore(align.score, l, k)
+                #     print(new_bitscore)
+
+                # print(calc_bitscore(align.score, l=0.201, k=0.012))
+                # print(bitscore)
+
     return nodes, edges
 
 def dataset_analysis(nodes, edges):
@@ -120,43 +157,26 @@ def dataset_analysis(nodes, edges):
     
     return num_seqs, score_by_len, score_by_id, score_by_edge
 
+def pickle_dict(d, fname):
+    with open(fname, 'wb') as fout:
+        pickle.dump(d, fout)
 
-def write_dicts(network_name, nodes, edges, num_seqs, score_by_len, score_by_id, score_by_edge):
+def write_all_dicts(network_name, nodes, edges, num_seqs, score_by_len, score_by_id, score_by_edge):
 
-    with open(f'out/{network_name}_nodes.py', 'w') as fout:
-        fout.write('nodes = {\n')
-        for k,v in nodes.items():
-            fout.write(f'    {k}: {v},\n')
-        fout.write('}\n\n')
+    # # this would also work, but would create multiple files. 
+    # pickle_dict(nodes, f'{network_name}_nodes.pickle')
+    # pickle_dict(edges, f'{network_name}_edges.pickle')
 
-    with open(f'out/{network_name}_edges.py', 'w') as fout:
-        fout.write('edges = {\n')
-        for k,v in edges.items():
-            fout.write(f'    {k}: {v},\n')
-        fout.write('}\n\n')
+    dict_of_dicts = {
+        'nodes': nodes,
+        'edges': edges,
+        'num_seqs': num_seqs,
+        'score_by_len': score_by_len,
+        'score_by_id': score_by_id,
+        'score_by_edge': score_by_edge
+    }
 
-    with open(f'out/{network_name}_data.py', 'w') as fout:
-        fout.write('num_seqs = {\n')
-        for k,v in num_seqs.items():
-            fout.write(f'    {k}: {v},\n')
-        fout.write('}\n\n')
-
-        fout.write('score_by_len = {\n')
-        for k,v in score_by_len.items():
-            fout.write(f'    {k}: {v},\n')
-        fout.write('}\n\n')
-
-        fout.write('score_by_id = {\n')
-        for k,v in score_by_id.items():
-            fout.write(f'    {k}: {v},\n')
-        fout.write('}\n\n')
-
-        fout.write('score_by_edge = {\n')
-        for k,v in score_by_edge.items():
-            fout.write(f'    {k}: {v},\n')
-        fout.write('}\n\n')
-
-
+    pickle_dict(dict_of_dicts, f'{network_name}_data.pickle')
 
 def write_xgmml(network_name, nodes, edges, score_cutoff):
 
@@ -194,8 +214,8 @@ def write_xgmml(network_name, nodes, edges, score_cutoff):
 
 if __name__ == '__main__':
     nodes, edges = fasta_to_dicts(fasta_file)
-    network_name = fasta_file.split('.fasta')[0].strip()
+    network_name = fasta_file.split('/')[-1].split('.fasta')[0].strip()
     num_seqs, score_by_len, score_by_id, score_by_edge = dataset_analysis(nodes, edges)
 
-    write_xgmml(network_name, nodes, edges, score_cutoff=75)
-    write_dicts(network_name, nodes, edges, num_seqs, score_by_len, score_by_id, score_by_edge)
+    # write_xgmml(network_name, nodes, edges, score_cutoff=90)
+    write_all_dicts(network_name, nodes, edges, num_seqs, score_by_len, score_by_id, score_by_edge)
