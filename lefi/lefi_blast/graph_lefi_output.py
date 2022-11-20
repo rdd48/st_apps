73b7baa9ad9567e369da90580865d6177950d089
Robby Divine <rdivine48@gmail.com>
2022-11-20 10:14:44 -0800

    trying to remove plotly for python3.10 version, since pip isnt installing plotly now

--- lefi/lefi_blast/graph_lefi_output.py
+++ lefi/lefi_blast/graph_lefi_output.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
-import plotly.express as px
-import plotly.graph_objects as go
+# import plotly.express as px
+# import plotly.graph_objects as go
 
 st.set_page_config(
     page_title='Local EFI',
@@ -82,117 +82,117 @@ def write_xgmml(network_name, nodes, edges, score_cutoff):
 
 uploaded_pickle = st.file_uploader('Upload the .pickle output from the initial dataset generation.')
 
-if uploaded_pickle is not None:
-    data = pickle.load(uploaded_pickle)
+# if uploaded_pickle is not None:
+#     data = pickle.load(uploaded_pickle)
 
-    sorted_scores = sorted(data['score_by_len'].keys())
-    if len(sorted_scores) > 1:
-        score_cutoff = st.slider(
-            'Set the score cutoff for each edge', 
-            min_value=sorted_scores[0], 
-            max_value=sorted_scores[-1],
-            value=sorted_scores[len(sorted_scores) // 2]
-            )
-    else:
-        score_cutoff = sorted_scores[0]
-        st.markdown(f'The only score is {score_cutoff}!')
+#     sorted_scores = sorted(data['score_by_len'].keys())
+#     if len(sorted_scores) > 1:
+#         score_cutoff = st.slider(
+#             'Set the score cutoff for each edge', 
+#             min_value=sorted_scores[0], 
+#             max_value=sorted_scores[-1],
+#             value=sorted_scores[len(sorted_scores) // 2]
+#             )
+#     else:
+#         score_cutoff = sorted_scores[0]
+#         st.markdown(f'The only score is {score_cutoff}!')
 
-    network_name = uploaded_pickle.name.replace('.pickle', '.xgmml')
-    fout, num_edges = write_xgmml(network_name, data['nodes'], data['edges'], score_cutoff)
+#     network_name = uploaded_pickle.name.replace('.pickle', '.xgmml')
+#     fout, num_edges = write_xgmml(network_name, data['nodes'], data['edges'], score_cutoff)
 
-    st.markdown(f'**{num_edges}** edges using a score cutoff of {score_cutoff}')
-    st.download_button(
-        'Download .xgmml for Cytoscape graphing',
-        fout,
-        file_name=network_name
-        )
+#     st.markdown(f'**{num_edges}** edges using a score cutoff of {score_cutoff}')
+#     st.download_button(
+#         'Download .xgmml for Cytoscape graphing',
+#         fout,
+#         file_name=network_name
+#         )
 
-    max_bins = max(data['num_seqs'].keys())
-    bin_size = st.sidebar.number_input(
-        'Select the number of bins for the histograms', 
-        min_value=10 if max_bins // 5 > 10 else 1, 
-        max_value=max_bins // 2,
-        value=max_bins // 5
-    )
+#     max_bins = max(data['num_seqs'].keys())
+#     bin_size = st.sidebar.number_input(
+#         'Select the number of bins for the histograms', 
+#         min_value=10 if max_bins // 5 > 10 else 1, 
+#         max_value=max_bins // 2,
+#         value=max_bins // 5
+#     )
 
-    resized_ns = resize_by_num_boxes(data['num_seqs'], bin_size)
-    seq_len_hist = px.bar(
-        x = resized_ns.keys(),
-        y = resized_ns.values(),
-        labels={'x':'Sequence Length', 'y':'Count'})
+#     resized_ns = resize_by_num_boxes(data['num_seqs'], bin_size)
+#     seq_len_hist = px.bar(
+#         x = resized_ns.keys(),
+#         y = resized_ns.values(),
+#         labels={'x':'Sequence Length', 'y':'Count'})
 
-    score_len_hist = go.Figure()
-    resized_sbl = resize_by_num_boxes_boxplot(data['score_by_len'], bin_size)
-    for k in sorted(resized_sbl.keys()):
-        marker_color = 'lightblue' if k < score_cutoff else 'royalblue'
-        score_len_hist.add_trace(go.Box(
-            y=resized_sbl[k],
-            name=k,
-            marker_color=marker_color,
-        ))
-    score_len_hist.update_layout(
-        xaxis={'title': 'Alignment Score'},
-        yaxis={'title': 'Alignment Length'},
-        showlegend=False
-    )
+#     score_len_hist = go.Figure()
+#     resized_sbl = resize_by_num_boxes_boxplot(data['score_by_len'], bin_size)
+#     for k in sorted(resized_sbl.keys()):
+#         marker_color = 'lightblue' if k < score_cutoff else 'royalblue'
+#         score_len_hist.add_trace(go.Box(
+#             y=resized_sbl[k],
+#             name=k,
+#             marker_color=marker_color,
+#         ))
+#     score_len_hist.update_layout(
+#         xaxis={'title': 'Alignment Score'},
+#         yaxis={'title': 'Alignment Length'},
+#         showlegend=False
+#     )
 
-    score_id_hist = go.Figure()
-    resized_sbi = resize_by_num_boxes_boxplot(data['score_by_id'], bin_size)
-    for k in sorted(resized_sbi.keys()):
-        marker_color = 'lightblue' if k < score_cutoff else 'royalblue'
-        score_id_hist.add_trace(go.Box(
-            y=resized_sbi[k],
-            name=k,
-            marker_color=marker_color,
-        ))
-    score_id_hist.update_layout(
-        xaxis={'title': 'Alignment Score'},
-        yaxis={'title': 'Percent Identity'},
-        showlegend=False
-    )
+#     score_id_hist = go.Figure()
+#     resized_sbi = resize_by_num_boxes_boxplot(data['score_by_id'], bin_size)
+#     for k in sorted(resized_sbi.keys()):
+#         marker_color = 'lightblue' if k < score_cutoff else 'royalblue'
+#         score_id_hist.add_trace(go.Box(
+#             y=resized_sbi[k],
+#             name=k,
+#             marker_color=marker_color,
+#         ))
+#     score_id_hist.update_layout(
+#         xaxis={'title': 'Alignment Score'},
+#         yaxis={'title': 'Percent Identity'},
+#         showlegend=False
+#     )
 
-    reverse_scores = sorted(data['score_by_edge'].keys(), reverse=True)
-    y_edges = []
-    curr_edge_num = 0
-    for s in reverse_scores:
-        curr_edge_num += data['score_by_edge'][s]
-        y_edges.append(curr_edge_num)
+#     reverse_scores = sorted(data['score_by_edge'].keys(), reverse=True)
+#     y_edges = []
+#     curr_edge_num = 0
+#     for s in reverse_scores:
+#         curr_edge_num += data['score_by_edge'][s]
+#         y_edges.append(curr_edge_num)
 
-    align_score_line = px.line(
-        x = reverse_scores,
-        y = y_edges,
-        labels={'x':'Alignment Score', 'y':'Number of Edges (Cumulative)'},
-        # marker_color=num_edge_colors
-    )
+#     align_score_line = px.line(
+#         x = reverse_scores,
+#         y = y_edges,
+#         labels={'x':'Alignment Score', 'y':'Number of Edges (Cumulative)'},
+#         # marker_color=num_edge_colors
+#     )
 
-    edge_count_hist = go.Figure()
-    resized_sbe = resize_by_num_boxes(data['score_by_edge'], bin_size)
-    for k in sorted(resized_sbe.keys()):
-        marker_color = 'lightblue' if k < score_cutoff else 'royalblue'
-        edge_count_hist.add_trace(go.Bar(
-            x=[k], # these have to be lists? dumb
-            y=[resized_sbe[k]], # these have to be lists? dumb
-            name=k,
-            marker_color=marker_color,
-        ))
-    edge_count_hist.update_layout(
-        xaxis={'title': 'Alignment Score'},
-        yaxis={'title': 'Number of Edges (per bin)'},
-        showlegend=False
-    )
+#     edge_count_hist = go.Figure()
+#     resized_sbe = resize_by_num_boxes(data['score_by_edge'], bin_size)
+#     for k in sorted(resized_sbe.keys()):
+#         marker_color = 'lightblue' if k < score_cutoff else 'royalblue'
+#         edge_count_hist.add_trace(go.Bar(
+#             x=[k], # these have to be lists? dumb
+#             y=[resized_sbe[k]], # these have to be lists? dumb
+#             name=k,
+#             marker_color=marker_color,
+#         ))
+#     edge_count_hist.update_layout(
+#         xaxis={'title': 'Alignment Score'},
+#         yaxis={'title': 'Number of Edges (per bin)'},
+#         showlegend=False
+#     )
 
-    st.markdown('Node dataset analysis')
-    st.plotly_chart(seq_len_hist)
+#     st.markdown('Node dataset analysis')
+#     st.plotly_chart(seq_len_hist)
 
-    st.markdown('Edge dataset analysis')
-    st.plotly_chart(score_len_hist)
-    st.plotly_chart(score_id_hist)
+#     st.markdown('Edge dataset analysis')
+#     st.plotly_chart(score_len_hist)
+#     st.plotly_chart(score_id_hist)
     
-    display_vline = st.checkbox('Display score cutoff on below graph?', value=True)
-    if display_vline:
-        align_score_line.add_vline(x=score_cutoff)
-    st.plotly_chart(align_score_line)
+#     display_vline = st.checkbox('Display score cutoff on below graph?', value=True)
+#     if display_vline:
+#         align_score_line.add_vline(x=score_cutoff)
+#     st.plotly_chart(align_score_line)
 
-    st.plotly_chart(edge_count_hist)
+#     st.plotly_chart(edge_count_hist)
 
 
