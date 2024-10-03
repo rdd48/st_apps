3c54c20149be72b8abff38bd8dad3e3209105eab
Robby Divine <rdivine48@gmail.com>
2024-10-03 15:38:48 -0700

    trying to fix a numpy version bug

--- lefi/graph_lefi_output.py
+++ lefi/graph_lefi_output.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pickle
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
+np.bool = np.bool_
 
 st.set_page_config(
     page_title='Local EFI',
